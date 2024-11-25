c27e388c00c0cffe126b8b539aec52735b3e2968
Leonel Aranda <leo.aranda@duocuc.cl>
2024-11-25 15:01:57 -0300

    cors2

--- src/main/java/cl/capstone/ms_gestion_trabajadores/Controller/TrabajadorFaenaController.java
+++ src/main/java/cl/capstone/ms_gestion_trabajadores/Controller/TrabajadorFaenaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import cl.capstone.ms_gestion_trabajadores.model.TrabajadorFaena;
 import cl.capstone.ms_gestion_trabajadores.service.ITrabajadorFaenaService;
 
 @RestController
-
+@CrossOrigin(origins = "*", allowedHeaders = "*") // CORS para todos los endpoints en esta clase
 public class TrabajadorFaenaController {
 
     @Autowired
